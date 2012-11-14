50f71d00d7c5174dae881a5ee89ed25d49296046
Michael Buesch <m@bues.ch>
2012-11-14 12:19:10 +0100

    Extend parser for blocks
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlparser.py
+++ awlparser.py
@@ -67,18 +67,62 @@ class RawAwlInsn(object):
 	def hasOperators(self):
 		return bool(self.getOperators())
 
+class RawAwlBlock(object):
+	def __init__(self, index):
+		self.index = index
+
+class RawAwlDB(RawAwlBlock):
+	def __init__(self, index):
+		RawAwlBlock.__init__(self, index)
+
+class RawAwlOB(RawAwlBlock):
+	def __init__(self, index):
+		RawAwlBlock.__init__(self, index)
+		self.insns = []
+
+class RawAwlFB(RawAwlBlock):
+	def __init__(self, index):
+		RawAwlBlock.__init__(self, index)
+		self.insns = []
+
+class RawAwlFC(RawAwlBlock):
+	def __init__(self, index):
+		RawAwlBlock.__init__(self, index)
+		self.insns = []
+
 class AwlParseTree(object):
 	def __init__(self):
-		pass#TODO
+		self.dbs = {}
+		self.fbs = {}
+		self.fcs = {}
+		self.obs = {}
+
+		self.curBlockIndex = None
 
 class AwlParser(object):
+	STATE_GLOBAL		= 0
+	STATE_IN_DB_HDR		= 1
+	STATE_IN_DB		= 2
+	STATE_IN_FB_HDR		= 3
+	STATE_IN_FB		= 4
+	STATE_IN_FC_HDR		= 5
+	STATE_IN_FC		= 6
+	STATE_IN_OB_HDR		= 7
+	STATE_IN_OB		= 8
+
 	def __init__(self):
 		self.reset()
 
 	def reset(self):
-		self.insns = []
+		self.state = self.STATE_GLOBAL
+		self.flatLayout = True
+		self.tree = AwlParseTree()
 
-	def __parseLine(self, line, lineNr):
+	def __setState(self, newState):
+		self.state = newState
+		self.flatLayout = False
+
+	def __parseLine(self, line):
 		line = line.strip()
 		if not line:
 			return None
@@ -114,40 +158,78 @@ class AwlParser(object):
 			tokens.append(curToken)
 		if inQuote:
 			raise AwlParserError("Unterminated quote: " + line)
-		return self.__parseTokens(line, tokens, lineNr)
-
-	def __parseTokens(self, line, tokens, lineNr):
 		if not tokens:
 			return None
+
+		if self.state == self.STATE_GLOBAL:
+			return self.__parseTokens_global(line, tokens)
+		if self.state == self.STATE_IN_DB_HDR:
+			return self.__parseTokens_db_hdr(line, tokens)
+		if self.state == self.STATE_IN_DB:
+			return self.__parseTokens_db(line, tokens)
+		if self.state == self.STATE_IN_FB_HDR:
+			return self.__parseTokens_fb_hdr(line, tokens)
+		if self.state == self.STATE_IN_FB:
+			return self.__parseTokens_fb(line, tokens)
+		if self.state == self.STATE_IN_FC_HDR:
+			return self.__parseTokens_fc_hdr(line, tokens)
+		if self.state == self.STATE_IN_FC:
+			return self.__parseTokens_fc(line, tokens)
+		if self.state == self.STATE_IN_OB_HDR:
+			return self.__parseTokens_ob_hdr(line, tokens)
+		if self.state == self.STATE_IN_OB:
+			return self.__parseTokens_ob(line, tokens)
+		assert(0)
+
+	def __parseTokens_global(self, line, tokens):
+		try:
+			if tokens[0].upper() == "DATA_BLOCK":
+				self.__setState(self.STATE_IN_DB_HDR)
+				if tokens[1].upper() != "DB":
+					raise AwlParserError("Invalid DB name")
+				dbNumber = int(tokens[2], 10)
+				self.tree.dbs[dbNumber] = RawAwlDB(dbNumber)
+				self.tree.curBlockIndex = dbNumber
+				return
+			if tokens[0].upper() == "FUNCTION_BLOCK":
+				self.__setState(self.STATE_IN_FB_HDR)
+				if tokens[1].upper() != "FB":
+					raise AwlParserError("Invalid FB name")
+				fbNumber = int(tokens[2], 10)
+				self.tree.fbs[fbNumber] = RawAwlFB(fbNumber)
+				self.tree.curBlockIndex = fbNumber
+				return
+			if tokens[0].upper() == "FUNCTION":
+				self.__setState(self.STATE_IN_FC_HDR)
+				if tokens[1].upper() != "FC":
+					raise AwlParserError("Invalid FC name")
+				fcNumber = int(tokens[2], 10)
+				self.tree.fcs[fcNumber] = RawAwlFC(fcNumber)
+				self.tree.curBlockIndex = fcNumber
+				return
+			if tokens[0].upper() == "ORGANIZATION_BLOCK":
+				self.__setState(self.STATE_IN_OB_HDR)
+				if tokens[1].upper() != "OB":
+					raise AwlParserError("Invalid OB name")
+				obNumber = int(tokens[2], 10)
+				self.tree.obs[obNumber] = RawAwlOB(obNumber)
+				self.tree.curBlockIndex = obNumber
+				return
+		except IndexError as e:
+			raise AwlParserError("Missing token")
+		except ValueError as e:
+			raise AwlParserError("Invalid value")
+
+		if self.flatLayout:
+			insn = self.__parseInstruction(line, tokens)
+			self.tree.obs[1].insns.append(insn)
+			return
+
+		raise AwlParserError("Trailing foo")
+
+	def __parseInstruction(self, line, tokens):
 		insn = RawAwlInsn()
-		insn.setLineNr(lineNr)
-		if tokens[0].upper() == "FAMILY":
-			pass #TODO
-			return None
-		if tokens[0].upper() == "AUTHOR":
-			pass #TODO
-			return None
-		if tokens[0].upper() == "DATA_BLOCK":
-			pass #TODO
-			return None
-		if tokens[0].upper() == "FUNCTION":
-			pass #TODO
-			return None
-		if tokens[0].upper() == "TITLE":
-			pass #TODO
-			return None
-		if tokens[0].upper() == "VERSION":
-			pass #TODO
-			return None
-		if tokens[0].upper() == "BEGIN":
-			pass #TODO
-			return None
-		if tokens[0].upper() == "NETWORK":
-			pass #TODO
-			return None
-		if tokens[0].upper() == "END_FUNCTION":
-			pass #TODO
-			return None
+		insn.setLineNr(self.lineNr)
 		if tokens[0].endswith(":"):
 			# First token is a label
 			label = tokens[0][0:-1]
@@ -164,6 +246,78 @@ class AwlParser(object):
 			insn.setOperators(tokens)
 		return insn
 
+	def __parseTokens_db_hdr(self, line, tokens):
+		if tokens[0].upper() == "BEGIN":
+			self.__setState(self.STATE_IN_DB)
+			return
+		pass#TODO
+
+	def __parseTokens_db(self, line, tokens):
+		if tokens[0].upper() == "END_DATA_BLOCK":
+			self.__setState(self.STATE_GLOBAL)
+			return
+		pass#TODO
+
+	def __parseTokens_fb_hdr(self, line, tokens):
+		if tokens[0].upper() == "BEGIN":
+			self.__setState(self.STATE_IN_FB)
+			return
+		pass#TODO
+
+	def __parseTokens_fb(self, line, tokens):
+		if tokens[0].upper() == "END_FUNCTION_BLOCK":
+			self.__setState(self.STATE_GLOBAL)
+			return
+		if tokens[0].upper() == "NETWORK":
+			pass#TODO
+			return
+		if tokens[0].upper() == "TITLE":
+			pass#TODO
+			return
+		pass#TODO
+		insn = self.__parseInstruction(line, tokens)
+		self.tree.fbs[self.tree.curBlockIndex].insns.append(insn)
+
+	def __parseTokens_fc_hdr(self, line, tokens):
+		if tokens[0].upper() == "BEGIN":
+			self.__setState(self.STATE_IN_FC)
+			return
+		pass#TODO
+
+	def __parseTokens_fc(self, line, tokens):
+		if tokens[0].upper() == "END_FUNCTION":
+			self.__setState(self.STATE_GLOBAL)
+			return
+		if tokens[0].upper() == "NETWORK":
+			pass#TODO
+			return
+		if tokens[0].upper() == "TITLE":
+			pass#TODO
+			return
+		pass#TODO
+		insn = self.__parseInstruction(line, tokens)
+		self.tree.fcs[self.tree.curBlockIndex].insns.append(insn)
+
+	def __parseTokens_ob_hdr(self, line, tokens):
+		if tokens[0].upper() == "BEGIN":
+			self.__setState(self.STATE_IN_OB)
+			return
+		pass#TODO
+
+	def __parseTokens_ob(self, line, tokens):
+		if tokens[0].upper() == "END_ORGANIZATION_BLOCK":
+			self.__setState(self.STATE_GLOBAL)
+			return
+		if tokens[0].upper() == "NETWORK":
+			pass#TODO
+			return
+		if tokens[0].upper() == "TITLE":
+			pass#TODO
+			return
+		pass#TODO
+		insn = self.__parseInstruction(line, tokens)
+		self.tree.obs[self.tree.curBlockIndex].insns.append(insn)
+
 	def parseFile(self, filename):
 		try:
 			fd = open(filename, "r")
@@ -176,19 +330,18 @@ class AwlParser(object):
 
 	def parseData(self, data):
 		self.reset()
-		lineNr = 0
+		self.lineNr = 0
+		self.tree.obs[1] = RawAwlOB(1)
 		for line in data.splitlines():
-			lineNr += 1
+			self.lineNr += 1
 			line = line.strip()
 			if not line:
 				continue
-			insn = self.__parseLine(line, lineNr)
-			if not insn:
-				continue
-			self.insns.append(insn)
+			self.__parseLine(line)
 
 	def getRawInsns(self):
-		return self.insns
+		#TODO
+		return self.tree.obs[1].insns
 
 if __name__ == "__main__":
 	p = AwlParser()
