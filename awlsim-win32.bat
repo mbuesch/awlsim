513b7142162bccb4c22df58816beebce72cec01c
Michael Buesch <m@bues.ch>
2012-11-15 20:48:29 +0100

    Use py win32 wrapper in awlsim-win32.bat
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim-win32.bat
+++ awlsim-win32.bat
@@ -1,2 +1,41 @@
-set PATH=%PATH%;C:\WINDOWS;C:\WINDOWS\SYSTEM32;C:\PYTHON;C:\PYTHON34;C:\PYTHON33;C:\PYTHON32;C:\PYTHON31;C:\PYTHON30
-python.exe awlsimgui
+@ECHO OFF
+
+SET PATH=%PATH%;C:\WINDOWS;C:\WINDOWS\SYSTEM32;C:\PYTHON;C:\PYTHON33;C:\PYTHON32;C:\PYTHON31;C:\PYTHON30
+
+SET PYPROG=awlsimgui
+
+
+py -h >NUL 2>&1
+if %ERRORLEVEL% EQU 0 GOTO exec_py
+
+python3 -h >NUL 2>&1
+if %ERRORLEVEL% EQU 0 GOTO exec_python3
+
+python -h >NUL 2>&1
+if %ERRORLEVEL% EQU 0 GOTO exec_python
+
+echo Did not find Python 3.x in the PATH.
+echo Please make sure Python 3.x is installed correctly.
+pause
+goto end
+
+
+:exec_py
+@ECHO ON
+py -3 -O %PYPROG%
+GOTO end
+
+
+:exec_python3
+@ECHO ON
+python3 -O %PYPROG%
+GOTO end
+
+
+:exec_python
+@ECHO ON
+python -O %PYPROG%
+GOTO end
+
+
+:end
