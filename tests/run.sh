95b2b654c8a08d8376d499418973e745dfb91090
Michael Buesch <m@bues.ch>
2013-05-19 11:18:28 +0200

    Tests: Add Python2 and PyPy support
    Signed-off-by: Michael Buesch <m@bues.ch>

--- tests/run.sh
+++ tests/run.sh
@@ -9,22 +9,33 @@ die()
 	exit 1
 }
 
-# $1=awl_file
+# $1=interpreter $2=awl_file
 run_test()
 {
-	local awl="$1"
+	local interpreter="$1"
+	local awl="$2"
 
 	echo "Running test '$(basename "$awl")' ..."
-	"$basedir/../awlsimcli" --quiet --onecycle --extended-insns "$awl" ||\
+	"$interpreter" "$basedir/../awlsimcli" --quiet --onecycle --extended-insns "$awl" ||\
 		die "Test failed"
 }
 
-if [ $# -eq 0 ]; then
-	for awl in "$basedir"/*.awl; do
-		run_test "$awl"
-	done
-else
-	for opt in "$@"; do
-		run_test "$basedir/$(basename "$opt")"
-	done
-fi
+for interpreter in python3 python2.7 pypy; do
+	which "$interpreter" >/dev/null 2>&1 || {
+		echo "=== WARNING: '$interpreter' interpreter not found. Test skipped."
+		echo
+		continue
+	}
+
+	echo "=== Running tests with '$interpreter' interpreter."
+	if [ $# -eq 0 ]; then
+		for awl in "$basedir"/*.awl; do
+			run_test "$interpreter" "$awl"
+		done
+	else
+		for opt in "$@"; do
+			run_test "$interpreter" "$basedir/$(basename "$opt")"
+		done
+	fi
+	echo
+done
