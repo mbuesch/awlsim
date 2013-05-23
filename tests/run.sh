7e37f4868e398fc5b15ff03e006a1a244e907188
Michael Buesch <m@bues.ch>
2013-05-23 16:40:51 +0200

    testscript: Add timing information
    Signed-off-by: Michael Buesch <m@bues.ch>

--- tests/run.sh
+++ tests/run.sh
@@ -9,16 +9,56 @@ die()
 	exit 1
 }
 
+cleanup()
+{
+	[ -f "$test_time_file" ] && {
+		rm -f "$test_time_file"
+		test_time_file=
+	}
+}
+
 # $1=interpreter $2=awl_file
 run_test()
 {
 	local interpreter="$1"
 	local awl="$2"
 
-	echo "Running test '$(basename "$awl")' ..."
+	echo -n "Running test '$(basename "$awl")' ..."
+	command time -o "$test_time_file" -f '%E' \
 	"$interpreter" "$basedir/../awlsimcli" --quiet --onecycle --extended-insns "$awl" ||\
 		die "Test failed"
+	echo " [$(cat "$test_time_file")]"
 }
+
+# $@=testfiles
+do_tests()
+{
+	for interpreter in "$opt_interpreter" python3 python2.7 pypy; do
+		[ -z "$interpreter" ] && continue
+		which "$interpreter" >/dev/null 2>&1 || {
+			echo "=== WARNING: '$interpreter' interpreter not found. Test skipped."
+			echo
+			continue
+		}
+
+		echo "=== Running tests with '$interpreter' interpreter."
+		if [ $# -eq 0 ]; then
+			for awl in "$basedir"/*.awl; do
+				run_test "$interpreter" "$awl"
+			done
+		else
+			for opt in "$@"; do
+				run_test "$interpreter" "$basedir/$(basename "$opt")"
+			done
+		fi
+		echo
+
+		[ -n "$opt_interpreter" ] && break
+	done
+}
+
+trap cleanup EXIT INT TERM
+test_time_file="$(mktemp --tmpdir=/tmp awlsim-test-time.XXXXXX)"
 
 opt_interpreter=
 
@@ -40,25 +80,4 @@ while [ $# -ge 1 ]; do
 	shift
 done
 
-for interpreter in "$opt_interpreter" python3 python2.7 pypy; do
-	[ -z "$interpreter" ] && continue
-	which "$interpreter" >/dev/null 2>&1 || {
-		echo "=== WARNING: '$interpreter' interpreter not found. Test skipped."
-		echo
-		continue
-	}
-
-	echo "=== Running tests with '$interpreter' interpreter."
-	if [ $# -eq 0 ]; then
-		for awl in "$basedir"/*.awl; do
-			run_test "$interpreter" "$awl"
-		done
-	else
-		for opt in "$@"; do
-			run_test "$interpreter" "$basedir/$(basename "$opt")"
-		done
-	fi
-	echo
-
-	[ -n "$opt_interpreter" ] && break
-done
+do_tests "$@"
