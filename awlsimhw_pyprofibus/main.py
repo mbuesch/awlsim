7a08a0a8fb8cf31f3982517df574971b1fd39a4c
Michael Buesch <m@bues.ch>
2013-07-26 21:36:15 +0200

    Add basic profibus initialization code
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsimhw_pyprofibus/main.py
+++ awlsimhw_pyprofibus/main.py
@@ -23,25 +23,85 @@ from awlsim.hardware import *
 from awlsim.util import *
 
 try:
-	import pyprofibus.dp_master
+	import pyprofibus.dp_master as DPM
+	import pyprofibus.dp as DP
+	import pyprofibus.fdl as FDL
+	import pyprofibus.phy as PHY
 except (ImportError, RuntimeError) as e:
 	raise AwlSimError("Failed to import PROFIBUS protocol stack "
 		"module 'pyprofibus':\n%s" % str(e))
 
 
 class HardwareInterface(AbstractHardwareInterface):
-	name = "pyprofibus"
+	name = "PyProfibus"
 
 	def __init__(self, sim, parameters={}):
 		AbstractHardwareInterface.__init__(self,
 						   sim = sim,
 						   parameters = parameters)
+		self.__parseParameters()
+
+	def __parseParameters(self):
+		self.param_debug = self.getParam_int("debug", minValue = 0)
+		self.param_masterClass = self.getParam_int("masterClass",
+					defaultValue = 1,
+					minValue = 1, maxValue = 2)
+		self.param_masterAddr = self.getParam_int("masterAddr",
+					defaultValue = 1,
+					minValue = 0, maxValue = 126)
+		self.param_spiDev = self.getParam_int("spiDev", minValue = 0)
+		self.param_spiChip = self.getParam_int("spiChip", minValue = 0)
+
+	def __setupSlaves(self):
+		#TODO: Rewrite. Must be configurable.
+		et200s = DPM.DpSlaveDesc(identNumber = 0x806A,
+					 slaveAddr = 8)
+		for elem in (DP.DpCfgDataElement(0),
+			     DP.DpCfgDataElement(0x20),
+			     DP.DpCfgDataElement(0x20),
+			     DP.DpCfgDataElement(0x10)):
+			et200s.chkCfgTelegram.addCfgDataElement(elem)
+		et200s.setPrmTelegram.addUserPrmData([0x11 | 0x40])
+		et200s.setSyncMode(True)
+		et200s.setFreezeMode(True)
+		et200s.setGroupMask(1)
+		et200s.setWatchdog(300)
+		self.master.addSlave(et200s)
+
+	def __cleanup(self):
+		if self.master:
+			self.master.destroy()
+		self.master = None
+		self.phy = None
 
 	def doStartup(self):
-		pass#TODO
+		self.phy = None
+		self.master = None
+		try:
+			self.phy = PHY.CpPhy(device = self.param_spiDev,
+					     chipselect = self.param_spiChip,
+					     debug = True if (self.param_debug >= 2) else False)
+			if self.param_masterClass == 1:
+				DPM_cls = DPM.DPM1
+			else:
+				DPM_cls = DPM.DPM2
+			self.master = DPM_cls(phy = self.phy,
+					      masterAddr = self.param_masterAddr,
+					      debug = True if (self.param_debug >= 1) else False)
+			self.__setupSlaves()
+			self.master.initialize()
+		except PHY.PhyError as e:
+			self.raiseException("PHY error: %s" % str(e))
+			self.__cleanup()
+		except DP.DpError as e:
+			self.raiseException("Profibus-DP error: %s" % str(e))
+			self.__cleanup()
+		except FDL.FdlError as e:
+			self.raiseException("Fieldbug Data Link error: %s" % str(e))
+			self.__cleanup()
 
 	def doShutdown(self):
-		pass#TODO
+		self.__cleanup()
 
 	def readInputs(self):
 		pass#TODO
