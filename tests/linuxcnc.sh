f931ca21de6415698c0b92ca644ff02534e1f438
Michael Buesch <m@bues.ch>
2014-05-13 21:08:47 +0200

    Move linuxcnc-hal-fake
    Signed-off-by: Michael Buesch <m@bues.ch>

--- tests/linuxcnc.sh
+++ tests/linuxcnc.sh
@@ -5,7 +5,7 @@ sh_test()
 	local interpreter="$1"
 
 	cd "$rootdir" || die "Failed to change to rootdir '$rootdir'"
-	PYTHONPATH="$rootdir/linuxcnc_fake_hal" \
+	PYTHONPATH="$rootdir/fake/linuxcnc_fake_hal" \
 		"$interpreter" ./awlsim-linuxcnc-hal \
 		--check-cnc 0 --onecycle EXAMPLE.awl
 }
