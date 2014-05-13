7a8e85daa6bdd5ccc69f78d0232f7e2b17d820b9
Michael Buesch <m@bues.ch>
2014-05-13 21:01:26 +0200

    linuxcnc-test: No export
    Signed-off-by: Michael Buesch <m@bues.ch>

--- tests/linuxcnc.sh
+++ tests/linuxcnc.sh
@@ -5,6 +5,7 @@ sh_test()
 	local interpreter="$1"
 
 	cd "$rootdir" || die "Failed to change to rootdir '$rootdir'"
-	export PYTHONPATH="$rootdir/linuxcnc_fake_hal"
-	"$interpreter" ./awlsim-linuxcnc-hal --check-cnc 0 --onecycle EXAMPLE.awl
+	PYTHONPATH="$rootdir/linuxcnc_fake_hal" \
+		"$interpreter" ./awlsim-linuxcnc-hal \
+		--check-cnc 0 --onecycle EXAMPLE.awl
 }
