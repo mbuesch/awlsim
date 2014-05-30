421a9d0e988aa1ab06a171544b582395d556de77
Michael Buesch <m@bues.ch>
2014-05-30 12:14:11 +0200

    Add support for forcing Cython usage.
    Set AWLSIMCYTHON=2 to force Cython usage.
    
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/cython_helper.py
+++ awlsim/cython_helper.py
@@ -1,5 +1,8 @@
 from __future__ import division, absolute_import, print_function, unicode_literals
 
+__USE_CYTHON_NO		= 0 # Do not use Cython.
+__USE_CYTHON_TRY	= 1 # Try to use Cython, if available.
+__USE_CYTHON_FORCE	= 2 # Use Cython. Abort if not available.
 __useCython = None
 
 def shouldUseCython():
@@ -8,17 +11,31 @@ def shouldUseCython():
 
 	if __useCython is None:
 		try:
-			__useCython = bool(int(os.getenv("AWLSIMCYTHON", "0")))
+			__useCython = int(os.getenv("AWLSIMCYTHON",
+						    str(__USE_CYTHON_NO)))
+			if __useCython not in (__USE_CYTHON_NO,
+					       __USE_CYTHON_TRY,
+					       __USE_CYTHON_FORCE):
+				raise ValueError
 		except ValueError:
-			__useCython = False
+			__useCython = __USE_CYTHON_NO
 	return __useCython
 
 def cythonImportError(modname, message):
 	global __useCython
 	import sys
 
-	sys.stderr.write("WARNING: Failed to import awlsim CYTHON module '%s': "
-			 "%s\n" % (modname, message))
-	sys.stderr.write("--> Falling back to standard Python modules...\n")
-	sys.stderr.flush()
-	__useCython = False
+	if __useCython == __USE_CYTHON_TRY:
+		sys.stderr.write("WARNING: Failed to import awlsim CYTHON module '%s': "
+				 "%s\n" % (modname, message))
+		sys.stderr.write("--> Falling back to standard Python modules...\n")
+		sys.stderr.flush()
+		__useCython = False
+	elif __useCython == __USE_CYTHON_FORCE:
+		sys.stderr.write("ERROR: Failed to import awlsim CYTHON module '%s': "
+				 "%s\n" % (modname, message))
+		sys.stderr.write("Aborting.\n")
+		sys.stderr.flush()
+		sys.exit(1)
+	else:
+		assert(0)
