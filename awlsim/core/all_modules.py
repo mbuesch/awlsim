7ac04854a03f2031f463758f00e28bf969aa7624
Michael Buesch <m@bues.ch>
2014-08-18 18:17:39 +0200

    Rename all.py
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ awlsim/core/all_modules.py
@@ -0,0 +1,10 @@
+from __future__ import division, absolute_import, print_function, unicode_literals
+from awlsim.core.compat import *
+
+from awlsim.core.main import *
+from awlsim.core.cpu import *
+from awlsim.core.hardware import *
+from awlsim.core.parser import *
+from awlsim.core.project import *
+from awlsim.core.util import *
+from awlsim.core.version import *
---
+++ awlsim/coreserver/all_modules.py
@@ -0,0 +1,5 @@
+from __future__ import division, absolute_import, print_function, unicode_literals
+from awlsim.core.compat import *
+
+from awlsim.coreserver.client import *
+from awlsim.coreserver.server import *
