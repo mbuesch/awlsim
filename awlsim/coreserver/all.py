656c6b2ae9530f953fd11e806bad6c7b43925a2f
Michael Buesch <m@bues.ch>
2014-08-18 18:18:10 +0200

    Remove all.py
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/coreserver/all.py
+++
@@ -1,5 +0,0 @@
-from __future__ import division, absolute_import, print_function, unicode_literals
-from awlsim.core.compat import *
-
-from awlsim.coreserver.client import *
-from awlsim.coreserver.server import *
