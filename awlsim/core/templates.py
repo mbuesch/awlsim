230acca5724e7eb22a8ee98c1abfabecb599428d
Michael Buesch <m@bues.ch>
2014-08-21 19:00:34 +0200

    templates: Add more OB temp vars
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/core/templates.py
+++ awlsim/core/templates.py
@@ -29,18 +29,7 @@ class Templates(object):
 	TITLE		= Insert title here
 	AUTHOR		: Insert author name here
 	VERSION		: 0.1
-	VAR_TEMP
-		OB1_EV_CLASS	: BYTE;		// Bits 0-3 = 1 (Coming event), Bits 4-7 = 1 (Event class 1)
-		OB1_SCAN_1	: BYTE;		// 1 (Cold restart scan 1 of OB 1), 3 (Scan 2-n of OB 1)
-		OB1_PRIORITY	: BYTE;		// Priority of OB Execution
-		OB1_OB_NUMBR	: BYTE;		// 1 (Organization block 1, OB1)
-		OB1_RESERVED_1	: BYTE;
-		OB1_RESERVED_2	: BYTE;
-		OB1_PREV_CYCLE	: INT;		// Cycle time of previous OB1 scan (milliseconds)
-		OB1_MIN_CYCLE	: INT;		// Minimum cycle time of OB1 (milliseconds)
-		OB1_MAX_CYCLE	: INT;		// Maximum cycle time of OB1 (milliseconds)
-		OB1_DATE_TIME	: DATE_AND_TIME;// Date and time OB1 started
-
+	VAR_TEMP@@TEMPVARS@@
 		// ... Insert temporary variables here ...
 	END_VAR
 BEGIN
@@ -51,6 +40,182 @@ NETWORK
 
 END_ORGANIZATION_BLOCK
 """
+
+	# OB start info variables
+	__obTempVars = (
+		( range(1, 1 + 1), """
+		OB@@NR@@_EV_CLASS   : BYTE;          // Bits 0-3 = 1 (Coming event), Bits 4-7 = 1 (Event class 1)
+		OB@@NR@@_SCAN_1     : BYTE;          // 1 (Cold restart scan 1 of OB @@NR@@), 3 (Scan 2-n of OB @@NR@@)
+		OB@@NR@@_PRIORITY   : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR   : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1 : BYTE;
+		OB@@NR@@_RESERVED_2 : BYTE;
+		OB@@NR@@_PREV_CYCLE : INT;           // Cycle time of previous OB @@NR@@ scan (milliseconds)
+		OB@@NR@@_MIN_CYCLE  : INT;           // Minimum cycle time of OB @@NR@@ (milliseconds)
+		OB@@NR@@_MAX_CYCLE  : INT;           // Maximum cycle time of OB @@NR@@ (milliseconds)
+		OB@@NR@@_DATE_TIME  : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(10, 17 + 1), """
+		OB@@NR@@_EV_CLASS   : BYTE;          //Bits 0-3 = 1 (Coming event), Bits 4-7 = 1 (Event class 1)
+		OB@@NR@@_STRT_INFO  : BYTE;          // B#16#11 (OB @@NR@@ has started)
+		OB@@NR@@_PRIORITY   : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR   : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1 : BYTE;
+		OB@@NR@@_RESERVED_2 : BYTE;
+		OB@@NR@@_PERIOD_EXE : WORD;          // Period of execution (once, per minute/hour/day/week/month/year)
+		OB@@NR@@_RESERVED_3 : INT;
+		OB@@NR@@_RESERVED_4 : INT;
+		OB@@NR@@_DATE_TIME  : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(20, 23 + 1), """
+		OB@@NR@@_EV_CLASS   : BYTE;          // Bits 0-3 = 1 (Coming event), Bits 4-7 = 1 (Event class 1)
+		OB@@NR@@_STRT_INF   : BYTE;          // B#16#21 (OB @@NR@@ has started)
+		OB@@NR@@_PRIORITY   : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR   : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1 : BYTE;
+		OB@@NR@@_RESERVED_2 : BYTE;
+		OB@@NR@@_SIGN       : WORD;          // Identifier input (SIGN) attached to SRT_DINT
+		OB@@NR@@_DTIME      : TIME;          // Delay time (DTIME) input to SRT_DINT instruction
+		OB@@NR@@_DATE_TIME  : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(30, 38 + 1), """
+		OB@@NR@@_EV_CLASS   : BYTE;          // Bits 0-3 = 1 (Coming event), Bits 4-7 = 1 (Event class 1)
+		OB@@NR@@_STRT_INF   : BYTE;          // B#16#31 (OB @@NR@@ has started)
+		OB@@NR@@_PRIORITY   : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR   : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1 : BYTE;
+		OB@@NR@@_RESERVED_2 : BYTE;
+		OB@@NR@@_PHS_OFFSET : INT;           // Phase offset (integer, milliseconds)
+		OB@@NR@@_RESERVED_3 : INT;
+		OB@@NR@@_EXC_FREQ   : INT;           // Frequency of execution (msec)
+		OB@@NR@@_DATE_TIME  : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(40, 47 + 1), """
+		OB@@NR@@_EV_CLASS   : BYTE;          // Bits 0-3 = 1 (Coming event), Bits 4-7 = 1 (Event class 1)
+		OB@@NR@@_STRT_INF   : BYTE;          // B#16#41 (OB @@NR@@ has started)
+		OB@@NR@@_PRIORITY   : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR   : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1 : BYTE;
+		OB@@NR@@_IO_FLAG    : BYTE;          // B#16#54 (input module), B#16#55 (output module)
+		OB@@NR@@_MDL_ADDR   : WORD;          // Base address of module initiating interrupt
+		OB@@NR@@_POINT_ADDR : DWORD;         // Interrupt status of the module
+		OB@@NR@@_DATE_TIME  : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(55, 57 + 1), """
+		OB@@NR@@_EV_CLASS   : BYTE;          // Bits 0-3 = 1 (Coming event), Bits 4-7 = 1 (Event class 1)
+		OB@@NR@@_STRT_INF   : BYTE;          // B#16#55 (OB @@NR@@ has started)
+		OB@@NR@@_PRIORITY   : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR   : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1 : BYTE;
+		OB@@NR@@_IO_FLAG    : BYTE;          // B#16#54 (input module), B#16#55 (output module)
+		OB@@NR@@_MDL_ADDR   : WORD;          // Base address of module initiating interrupt
+		OB@@NR@@_LEN        : BYTE;          // Length of information
+		OB@@NR@@_TYPE       : BYTE;          // Type of alarm
+		OB@@NR@@_SLOT       : BYTE;          // Slot
+		OB@@NR@@_SPEC       : BYTE;          // Specifier
+		OB@@NR@@_DATE_TIME  : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(60, 60 + 1), """
+		OB@@NR@@_EV_CLASS   : BYTE;          // Bits 0-3 = 1 (Coming event), Bits 4-7 = 1 (Event class 1)
+		OB@@NR@@_STRT_INF   : BYTE;          // B#16#61 (self generated), B#16#62 (external generated)
+		OB@@NR@@_PRIORITY   : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR   : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1 : BYTE;
+		OB@@NR@@_RESERVED_2 : BYTE;
+		OB@@NR@@_JOB        : INT;           // Job sign
+		OB@@NR@@_RESERVED_3 : INT;
+		OB@@NR@@_RESERVED_4 : INT;
+		OB@@NR@@_DATE_TIME  : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(61, 64 + 1), """
+		OB@@NR@@_EV_CLASS    : BYTE;          // Bits 0-3 = 1 (Coming event), Bits 4-7 = 1 (Event class 1)
+		OB@@NR@@_STRT_INF    : BYTE;          // B#16#36 (OB @@NR@@ has started)
+		OB@@NR@@_PRIORITY    : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR    : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1  : BYTE;
+		OB@@NR@@_RESERVED_2  : BYTE;
+		OB@@NR@@_GC_VIOL     : BOOL;          // Lost Global Control of DP system
+		OB@@NR@@_FIRST       : BOOL;          // First Execution after startup or HALT
+		OB@@NR@@_MISSED_EXEC : BYTE;          // Count of missed executions of OB @@NR@@ since it was last scheduled
+		OB@@NR@@_DP_ID       : BYTE;          // DP master system ID of the synchronous DP system
+		OB@@NR@@_RESERVED_3  : BYTE;
+		OB@@NR@@_RESERVED_4  : WORD;
+		OB@@NR@@_DATE_TIME   : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(70, 72 + 1), """
+		OB@@NR@@_EV_CLASS   : BYTE;          // Event class 7, module inserted/removed (8/9)
+		OB@@NR@@_FLT_ID     : BYTE;          // Fault identifcation code
+		OB@@NR@@_PRIORITY   : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR   : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1 : WORD;
+		OB@@NR@@_INFO_1     : WORD;          // Info 1
+		OB@@NR@@_INFO_2     : WORD;          // Info 2
+		OB@@NR@@_INFO_3     : WORD;          // Info 3
+		OB@@NR@@_DATE_TIME  : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(80, 87 + 1), """
+		OB@@NR@@_EV_CLASS     : BYTE;          // B#16#35, Event class 3, Entering event state, Internal fault event
+		OB@@NR@@_FLT_ID       : BYTE;          // Fault identifcation code
+		OB@@NR@@_PRIORITY     : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR     : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1   : BYTE;
+		OB@@NR@@_RESERVED_2   : BYTE;
+		OB@@NR@@_ERROR_INFO   : WORD;          // Error information on event
+		OB@@NR@@_ERR_EV_CLASS : BYTE;          // Class of event causing error
+		OB@@NR@@_ERR_EV_NUM   : BYTE;          // Number of event causing error
+		OB@@NR@@_OB_PRIORITY  : BYTE;          // Priority of OB causing error
+		OB@@NR@@_OB_NUM       : BYTE;          // Number of OB causing error
+		OB@@NR@@_DATE_TIME    : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(90, 90 + 1), """
+		OB@@NR@@_EV_CLASS   : BYTE;          // Bits 0-3 = 1 (Coming event), Bits 4-7 = 1 (Event class 1)
+		OB@@NR@@_STRT_INF   : BYTE;          // 91 Cold restart, 92 Delete block, 93 Load OB @@NR@@, 95 End background cycle
+		OB@@NR@@_PRIORITY   : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR   : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1 : BYTE;
+		OB@@NR@@_RESERVED_2 : BYTE;
+		OB@@NR@@_RESERVED_3 : INT;
+		OB@@NR@@_RESERVED_4 : INT;
+		OB@@NR@@_RESERVED_5 : INT;
+		OB@@NR@@_DATE_TIME  : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(100, 102 + 1), """
+		OB@@NR@@_EV_CLASS   : BYTE;          // B#16#13, Event class 1, Entering event state, Event logged in diagnostic buffer
+		OB@@NR@@_STRTUP     : BYTE;          // B#16#81/82/83/84 Method of startup
+		OB@@NR@@_PRIORITY   : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR   : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_RESERVED_1 : BYTE;
+		OB@@NR@@_RESERVED_2 : BYTE;
+		OB@@NR@@_STOP       : WORD;          // Event that caused CPU to stop (W#16#4XXX)
+		OB@@NR@@_STRT_INFO  : DWORD;         // Information on how system started
+		OB@@NR@@_DATE_TIME  : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(121, 121 + 1), """
+		OB@@NR@@_EV_CLASS   : BYTE;          // B#16#25, Event class 2, Entering event state, Internal fault event
+		OB@@NR@@_SW_FLT     : BYTE;          // Software programming fault
+		OB@@NR@@_PRIORITY   : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR   : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_BLK_TYPE   : BYTE;          // B#16#88/8A/8B/8C/8E Type of block fault occured in
+		OB@@NR@@_RESERVED_1 : BYTE;
+		OB@@NR@@_FLT_REG    : WORD;          // Specific register that caused fault
+		OB@@NR@@_BLK_NUM    : WORD;          // Number of block that programming fault occured in
+		OB@@NR@@_PRG_ADDR   : WORD;          // Address in block where programming fault occured
+		OB@@NR@@_DATE_TIME  : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+		( range(122, 122 + 1), """
+		OB@@NR@@_EV_CLASS  : BYTE;          // B#16#29, Event class 2, Entering event state, Internal fault event
+		OB@@NR@@_SW_FLT    : BYTE;          // Software error code
+		OB@@NR@@_PRIORITY  : BYTE;          // Priority of OB execution
+		OB@@NR@@_OB_NUMBR  : BYTE;          // @@NR@@ (Organization block @@NR@@, OB @@NR@@)
+		OB@@NR@@_BLK_TYPE  : BYTE;          // B#16#88/8C/8E Type of block fault occured in
+		OB@@NR@@_MEM_AREA  : BYTE;          // Memory area where access error occured
+		OB@@NR@@_MEM_ADDR  : WORD;          // Memory address where access error occured
+		OB@@NR@@_BLK_NUM   : WORD;          // Block number in which error occured
+		OB@@NR@@_PRG_ADDR  : WORD;          // Program address where error occured
+		OB@@NR@@_DATE_TIME : DATE_AND_TIME; // Date and time OB @@NR@@ started"""
+		),
+	)
+
 
 	# FC template
 	__templateFC = """FUNCTION FC @@NR@@ : VOID
@@ -156,6 +321,13 @@ END_DATA_BLOCK
 	def getOB(cls, number):
 		awl = cls.__templateOB[:]
 		awl = awl.replace("@@NR@@", "%d" % number)
+		for rnge, tempvars in cls.__obTempVars:
+			if number in rnge:
+				tempvars = tempvars.replace("@@NR@@", "%d" % number)
+				awl = awl.replace("@@TEMPVARS@@", tempvars)
+				break
+		else:
+			awl = awl.replace("@@TEMPVARS@@", "")
 		return awl
 
 	@classmethod
