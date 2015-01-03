872ad14e1326cc4efaead5cfd998a968e22030ad
Michael Buesch <m@bues.ch>
2015-01-03 15:38:34 +0100

    tests: Add linuxcnc I/O test
    Signed-off-by: Michael Buesch <m@bues.ch>

--- tests/700-hardware/linuxcnc.sh
+++ tests/700-hardware/linuxcnc.sh
@@ -1,5 +1,25 @@
 # LinuxCNC hardware module test
 
+__run_awlsim_linuxcnc_hal()
+{
+	local interpreter="$1"
+	local test_dir="$2"
+	local awl_file="$3"
+	shift 3
+
+	local modpath="$rootdir/fake/linuxcnc_fake_hal"
+
+	FAKEHAL_HALFILE="${test_dir}/linuxcnc.hal" \
+	PYTHONPATH="$modpath:$PYTHONPATH" \
+	JYTHONPATH="$modpath:$JYTHONPATH" \
+	IRONPYTHONPATH="$modpath:$IRONPYTHONPATH" \
+		"$interpreter" ./awlsim-linuxcnc-hal \
+		--watchdog off --extended-insns \
+		"$@" \
+		"$awl_file" >/dev/null ||\
+			test_failed "LinuxCNC test '$(basename "$awl_file")' failed"
+}
+
 sh_test()
 {
 	local interpreter="$1"
@@ -8,21 +28,18 @@ sh_test()
 
 	cd "$rootdir" || die "Failed to change to rootdir '$rootdir'"
 
-	modpath="$rootdir/fake/linuxcnc_fake_hal"
-
 	echo ""
 	for testfile in "000-base/empty.awl"\
 			"000-base/shutdown.awl"\
 			"000-base/EXAMPLE.awlpro"; do
 		echo "    Running linuxcnc test with: $testfile"
 
-		FAKEHAL_HALFILE="${test_dir}/linuxcnc.hal" \
-		PYTHONPATH="$modpath:$PYTHONPATH" \
-		JYTHONPATH="$modpath:$JYTHONPATH" \
-		IRONPYTHONPATH="$modpath:$IRONPYTHONPATH" \
-			"$interpreter" ./awlsim-linuxcnc-hal \
-			--watchdog off --max-runtime 1.0 --extended-insns \
-			"$rootdir/tests/$testfile" >/dev/null ||\
-				test_failed "LinuxCNC test '$testfile' failed"
+		__run_awlsim_linuxcnc_hal "$interpreter" "$test_dir" \
+			"$rootdir/tests/$testfile" \
+			--max-runtime 1.0
 	done
+
+	echo "    Running I/O test"
+	__run_awlsim_linuxcnc_hal "$interpreter" "$test_dir" \
+		"$test_dir/linuxcnc-iotest.awl__"
 }
