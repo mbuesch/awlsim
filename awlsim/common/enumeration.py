d76cebc39e444369ddd896e931ae1838b477ce78
Michael Buesch <m@bues.ch>
2015-06-05 23:00:22 +0200

    enum: Add assertion for itemAt sequence
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/common/enumeration.py
+++ awlsim/common/enumeration.py
@@ -2,7 +2,7 @@
 #
 # AWL simulator - utility functions
 #
-# Copyright 2012-2013 Michael Buesch <m@bues.ch>
+# Copyright 2012-2015 Michael Buesch <m@bues.ch>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -68,6 +68,7 @@ class MagicEnumerationGenerator(object):
 
 	def itemAt(self, number):
 		assert(self.__num is not None)
+		assert(number >= self.__num)
 		self.__num = number + 1
 		return number
 
