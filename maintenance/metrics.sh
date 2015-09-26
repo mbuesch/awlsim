b4ecbd481b79311c78a5d8e6c0edc512769a16ce
Michael Buesch <m@bues.ch>
2015-09-26 22:18:20 +0200

    metrics: Exclude icons
    Signed-off-by: Michael Buesch <m@bues.ch>

--- maintenance/metrics.sh
+++ maintenance/metrics.sh
@@ -10,7 +10,7 @@ basedir="$(dirname "$0")"
 
 set -e
 
-cloc --exclude-dir=build,dist,release-archives \
+cloc --exclude-dir=build,dist,release-archives,icons \
 	--read-lang-def="${basedir}/cloc-lang-awl.txt" \
 	--quiet --progress-rate=0 \
 	"$@"
