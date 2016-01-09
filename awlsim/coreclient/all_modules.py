f1660d7e66e170a0573dc36557a227956990d619
Michael Buesch <m@bues.ch>
2016-01-09 11:45:27 +0100

    coreclient: Add net address parser
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/coreclient/all_modules.py
+++ awlsim/coreclient/all_modules.py
@@ -2,3 +2,4 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 from awlsim.common.compat import *
 
 from awlsim.coreclient.client import *
+from awlsim.coreclient.util import *
---
+++ awlsim/coreclient/util.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+# AWL simulator - Networking utils
+#
+# Copyright 2016 Michael Buesch <m@bues.ch>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+from __future__ import division, absolute_import, print_function, unicode_literals
+from awlsim.common.compat import *
+
+from awlsim.common.exceptions import *
+
+
+def parseNetAddress(addrStr):
+	"""Parse a host:port networking address string.
+	"""
+	addrStr = addrStr.strip()
+	if addrStr.startswith("["):
+		end = addrStr.rfind("]")
+		if end <= 0:
+			raise AwlSimError("Invalid IPv6 address. "
+				"Missing closing bracket ']'.")
+		host = addrStr[1:end]
+		portStr = addrStr[end+1:]
+	else:
+		end = addrStr.find(":")
+		if end < 0:
+			host = addrStr
+			portStr = ""
+		else:
+			host = addrStr[:end]
+			portStr = addrStr[end:]
+	portStr = portStr.strip()
+	if portStr:
+		try:
+			print(portStr)
+			if not portStr.startswith(":"):
+				raise ValueError
+			port = int(portStr[1:])
+			if port < 0 or port > 0xFFFF:
+				raise ValueError
+		except ValueError as e:
+			raise AwlSimError("Invalid port number.")
+	else:
+		port = None
+	host = host.strip()
+	if not host:
+		raise AwlSimError("Missing host name or IPv4/6 address.")
+	return host, port
