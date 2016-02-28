7b046858ef5809f5f43769a911a9c7b3503aaf48
Michael Buesch <m@bues.ch>
2016-02-28 11:13:52 +0100

    libs/distutils: Fix spawn
    Signed-off-by: Michael Buesch <m@bues.ch>

--- libs/tiny_distutils/distutils/spawn.py
+++ libs/tiny_distutils/distutils/spawn.py
@@ -3,11 +3,13 @@ __all__ = [ "find_executable", ]
 import os
 
 def find_executable(executable, path=None):
-	import os.path, stat
-	if path is None:
-		path = os.environ.get("PATH", "")
-	for p in os.path.split(":"):
-		fullpath = os.path.join(p, executable)
-		if os.access(fullpath, stat.S_IXOTH):
-			return fullpath
+	if executable:
+		import os.path
+		if path is None:
+			path = os.getenv("PATH", "")
+		for p in path.split(":"):
+			fullpath = os.path.join(p, executable)
+			#FIXME this is also true for directories
+			if os.access(fullpath, os.X_OK):
+				return fullpath
 	return None
