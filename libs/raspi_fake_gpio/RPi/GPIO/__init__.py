b895ace508a0a9eee13b819b1e05ba0242ea9c08
Michael Buesch <m@bues.ch>
2016-03-17 20:22:27 +0100

    Add RPi.GPIO fake module for testing
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ libs/raspi_fake_gpio/RPi/GPIO/__init__.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+# Raspberry Pi fake GPIO module for unit testing
+#
+# Copyright 2016 Michael Buesch <m@bues.ch>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+from __future__ import division, absolute_import, print_function, unicode_literals
+
+
+BCM		= 11
+
+LOW		= 0
+HIGH		= 1
+
+OUT		= 0
+IN		= 1
+
+PUD_OFF		= 0 + 20
+PUD_DOWN	= 1 + 20
+PUD_UP		= 2 + 20
+
+__nrChannels = 28
+__state = [ LOW, ] * __nrChannels
+__directions = [ set(), ] * __nrChannels
+
+def setwarnings(enabled):
+	pass
+
+def setmode(mode):
+	assert(mode == BCM)
+
+def setup(channel, direction, pull_up_down=PUD_OFF, initial=None):
+	assert(pull_up_down in {PUD_OFF, PUD_DOWN, PUD_UP})
+	assert(initial in {None, LOW, HIGH})
+	assert(direction in {OUT, IN})
+	assert(channel >= 0 and channel < __nrChannels)
+	__directions[channel].add(direction)
+	if direction == OUT:
+		if initial is not None:
+			__state[channel] = initial
+
+def cleanup(channel = None):
+	pass
+
+def output(channels, values):
+	if isinstance(channels, int):
+		channels = [ channels, ]
+	if isinstance(values, int):
+		values = [ values, ] * len(channels)
+	for i, channel in enumerate(channels):
+		assert(isinstance(channel, int))
+		assert(isinstance(values[i], int) or\
+		       isinstance(values[i], bool))
+		assert(channel >= 0 and channel < __nrChannels)
+		assert(OUT in __directions[channel])
+		__state[channel] = HIGH if values[i] else LOW
+
+def input(channel):
+	assert(isinstance(channel, int))
+	assert(channel >= 0 and channel < __nrChannels)
+	assert(IN in __directions[channel])
+	return __state[channel]
