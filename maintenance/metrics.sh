338ba18dbda440382938fa2830ec4fcc5dd865b0
Michael Buesch <m@bues.ch>
2016-04-17 02:34:53 +0200

    metrics: Exclude submods
    Signed-off-by: Michael Buesch <m@bues.ch>

--- maintenance/metrics.sh
+++ maintenance/metrics.sh
@@ -10,7 +10,10 @@ basedir="$(dirname "$0")"
 
 set -e
 
-cloc --exclude-dir=build,dist,release-archives,icons \
+cd "$basedir/.."
+submodpaths="$(git submodule status | awk '{print $2;}' | tr "[:space:]" ",")"
+
+cloc --exclude-dir="build,dist,release-archives,icons,${submodpaths}" \
 	--read-lang-def="${basedir}/cloc-lang-awl.txt" \
 	--quiet --progress-rate=0 \
 	"$@"
