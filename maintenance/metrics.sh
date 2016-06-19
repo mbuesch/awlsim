aa615ff1d218fd2a7cc60ececffecbcabd1465f5
Michael Buesch <m@bues.ch>
2016-06-19 15:40:17 +0200

    cloc: Add more excludes

--- maintenance/metrics.sh
+++ maintenance/metrics.sh
@@ -13,7 +13,7 @@ set -e
 cd "$basedir/.."
 submodpaths="$(git submodule status | awk '{print $2;}' | tr "[:space:]" ",")"
 
-cloc --exclude-dir="build,dist,release-archives,icons,${submodpaths}" \
+cloc --exclude-dir="build,dist,.pybuild,release-archives,icons,__pycache__,${submodpaths},awlsim_cython,pyprofibus,libpilc" \
 	--read-lang-def="${basedir}/cloc-lang-awl.txt" \
 	--exclude-lang='ASP.Net,IDL,D' \
 	--quiet --progress-rate=0 \
