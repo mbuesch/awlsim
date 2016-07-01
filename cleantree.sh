e39a8a4217a38010ec6da46d3e3848b1140b6a97
Michael Buesch <m@bues.ch>
2016-07-01 21:55:05 +0200

    Update cleantree
    Signed-off-by: Michael Buesch <m@bues.ch>

--- cleantree.sh
+++ cleantree.sh
@@ -32,26 +32,34 @@ rm -rf debian/destdir-* \
        debian/python-awlsimhw-dummy \
        debian/python-awlsimhw-linuxcnc \
        debian/python-awlsimhw-profibus \
+       debian/python-awlsimhw-rpigpio \
        debian/python-awlsim-gui \
+       debian/python-libpilc \
        debian/cython-awlsim \
        debian/cython-awlsimhw-dummy \
        debian/cython-awlsimhw-linuxcnc \
        debian/cython-awlsimhw-profibus \
+       debian/cython-awlsimhw-rpigpio \
        debian/python3-awlsim \
        debian/python3-awlsim-gui \
+       debian/python3-libpilc \
        debian/python3-awlsimhw-dummy \
        debian/python3-awlsimhw-linuxcnc \
        debian/python3-awlsimhw-profibus \
+       debian/python3-awlsimhw-rpigpio \
        debian/cython3-awlsim \
        debian/cython3-awlsimhw-dummy \
        debian/cython3-awlsimhw-linuxcnc \
        debian/cython3-awlsimhw-profibus \
+       debian/cython3-awlsimhw-rpigpio \
        debian/pypy-awlsim \
        debian/pypy-awlsimhw-dummy \
        debian/pypy-awlsimhw-profibus \
+       debian/pypy-awlsimhw-rpigpio \
        debian/awlsim-client \
        debian/awlsim-server \
        debian/awlsim-symtab \
        debian/awlsim-test \
        debian/awlsim-linuxcnc-hal \
-       debian/awlsim-gui
+       debian/awlsim-gui \
+       debian/pilc-hat-conf
