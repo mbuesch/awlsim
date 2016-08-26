89733d90e639e0881528c7837d060fd213a1d799
Michael Buesch <m@bues.ch>
2016-08-26 21:20:07 +0200

    awlsim-win: Pass arguments
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim-win.cmd
+++ awlsim-win.cmd
@@ -1,21 +1,21 @@
-@ECHO OFF
+@echo off
 
-SET PYPROG=awlsim-gui
+set PYPROG=awlsim-gui
 
 
-SETLOCAL ENABLEDELAYEDEXPANSION
+setlocal ENABLEDELAYEDEXPANSION
 
-SET PATH=%PATH%;C:\WINDOWS;C:\WINDOWS\SYSTEM32
-FOR /D %%f IN ( C:\PYTHON* ) DO SET PATH=!PATH!;%%f
+set PATH=%PATH%;C:\WINDOWS;C:\WINDOWS\SYSTEM32
+for /D %%f in ( C:\PYTHON* ) do set PATH=!PATH!;%%f
 
 py -h >NUL 2>&1
-if %ERRORLEVEL% EQU 0 GOTO exec_py
+if %ERRORLEVEL% EQU 0 goto exec_py
 
 python3 -h >NUL 2>&1
-if %ERRORLEVEL% EQU 0 GOTO exec_python3
+if %ERRORLEVEL% EQU 0 goto exec_python3
 
 python -h >NUL 2>&1
-if %ERRORLEVEL% EQU 0 GOTO exec_python
+if %ERRORLEVEL% EQU 0 goto exec_python
 
 echo Did not find Python 3.x in the PATH.
 echo Please make sure Python 3.x is installed correctly.
@@ -24,21 +24,21 @@ goto end
 
 
 :exec_py
-@ECHO ON
-py -3 -O %PYPROG%
-GOTO end
+@echo on
+py -3 -O %PYPROG% %1 %2 %3 %4 %5 %6 %7 %8 %9
+@goto end
 
 
 :exec_python3
-@ECHO ON
-python3 -O %PYPROG%
-GOTO end
+@echo on
+python3 -O %PYPROG% %1 %2 %3 %4 %5 %6 %7 %8 %9
+@goto end
 
 
 :exec_python
-@ECHO ON
-python -O %PYPROG%
-GOTO end
+@echo on
+python -O %PYPROG% %1 %2 %3 %4 %5 %6 %7 %8 %9
+@goto end
 
 
 :end
