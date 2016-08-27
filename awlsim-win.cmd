3c762eeed237c7125ee39041f3a6ee3a5a40dd14
Michael Buesch <m@bues.ch>
2016-08-27 16:24:49 +0200

    win wrapper: Add putty path
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim-win.cmd
+++ awlsim-win.cmd
@@ -1,12 +1,14 @@
 @echo off
-
-set PYPROG=awlsim-gui
-
-
 setlocal ENABLEDELAYEDEXPANSION
 
+
+set PYPROG=awlsim-gui
+for /D %%f in ( "progs\putty\*" ) do set PATH=%%f\putty;!PATH!
+
+
+
 set PATH=%PATH%;C:\WINDOWS;C:\WINDOWS\SYSTEM32
-for /D %%f in ( C:\PYTHON* ) do set PATH=!PATH!;%%f
+for /D %%f in ( "C:\PYTHON*" ) do set PATH=!PATH!;%%f
 
 py -h >NUL 2>&1
 if %ERRORLEVEL% EQU 0 goto exec_py
