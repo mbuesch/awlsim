603fbbe36d8f8fbef5dda26e9b2918b07d94acc9
Michael Buesch <m@bues.ch>
2017-02-26 20:23:19 +0100

    Move cleantree.sh script to maintenance directory
    Signed-off-by: Michael Buesch <m@bues.ch>

--- maintenance/cleantree.sh
+++ maintenance/cleantree.sh
@@ -3,14 +3,16 @@
 basedir="$(dirname "$0")"
 [ "$(echo "$basedir" | cut -c1)" = '/' ] || basedir="$PWD/$basedir"
 
+awlsim_base="$basedir/.."
+
 set -e
 
-if ! [ -x "$basedir/awlsim-test" -a -x "$basedir/setup.py" ]; then
+if ! [ -x "$awlsim_base/awlsim-test" -a -x "$awlsim_base/setup.py" ]; then
 	echo "basedir sanity check failed"
 	exit 1
 fi
 
-cd "$basedir"
+cd "$awlsim_base"
 
 find . \( \
 	\( -name '__pycache__' \) -o \
