939ffcc0d545acb837f368594e768a0e2e7f7585
Michael Buesch <m@bues.ch>
2017-03-22 20:39:25 +0100

    tests: Add more nose wrappers
    Signed-off-by: Michael Buesch <m@bues.ch>

--- tests/awlsim_tstlib.py
+++ tests/awlsim_tstlib.py
@@ -3,7 +3,11 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 import nose as __nose
 
 assert_eq = __nose.tools.assert_equal
+assert_ne = __nose.tools.assert_not_equal
 assert_lt = __nose.tools.assert_less
 assert_le = __nose.tools.assert_less_equal
 assert_gt = __nose.tools.assert_greater
 assert_ge = __nose.tools.assert_greater_equal
+assert_is = __nose.tools.assert_is
+assert_is_not = __nose.tools.assert_is_not
+assert_is_none = __nose.tools.assert_is_none
