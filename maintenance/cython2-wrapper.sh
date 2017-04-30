158c4783b4c86d67fac6f45fa396413b5be4020a
Michael Buesch <m@bues.ch>
2017-04-30 21:56:40 +0200

    maintenance: Add cython wrappers
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ maintenance/cython2-wrapper.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+basedir="$(dirname "$0")"
+[ "$(echo "$basedir" | cut -c1)" = '/' ] || basedir="$PWD/$basedir"
+
+awlsim_base="$basedir/.."
+
+set -e
+
+if ! [ -x "$awlsim_base/awlsim-test" -a -x "$awlsim_base/setup.py" ]; then
+	echo "basedir sanity check failed"
+	exit 1
+fi
+
+cd "$awlsim_base"
+for path in ./build/lib.*-*-2.*; do
+	export PYTHONPATH="$path/:$PYTHONPATH"
+done
+export PYTHONPATH=".:$PYTHONPATH"
+export AWLSIM_CYTHON=2
+exec python2 "$@"
