7811022cedaa89d4ff9ae7c9846d7180cbe42eb3
Michael Buesch <m@bues.ch>
2017-04-30 15:59:09 +0200

    memory: Add missing offset import
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/core/memory.py
+++ awlsim/core/memory.py
@@ -23,6 +23,7 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 from awlsim.common.compat import *
 
 from awlsim.core.datatypes import *
+from awlsim.core.offset import * #+cimport
 
 
 class GenericInteger(object): #+cdef
