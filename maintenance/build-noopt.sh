51397cb0dcf1a0cb46f5ed11796fb50c9f89e5c4
Michael Buesch <m@bues.ch>
2017-06-07 20:21:28 +0200

    maintenance: Add script to build without optimization
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ maintenance/build-noopt.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+basedir="$(dirname "$0")"
+[ "$(echo "$basedir" | cut -c1)" = '/' ] || basedir="$PWD/$basedir"
+
+awlsim_base="$basedir/.."
+
+set -e
+
+if ! [ -x "$awlsim_base/awlsim-test" -a -x "$awlsim_base/setup.py" ]; then
+	echo "basedir sanity check failed"
+	exit 1
+fi
+
+export CFLAGS="$CFLAGS -O0"
+exec "$awlsim_base/maintenance/build.sh"
