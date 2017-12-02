ee875363bbfb0e4bd4caf75e44c1c037e8ba8c8c
Michael Buesch <m@bues.ch>
2017-12-02 23:36:41 +0100

    Add helper script to install dependencies on Debian
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ maintenance/deb-dependencies-install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+exec apt install \
+	python \
+	python-dev \
+	python3 \
+	python3-dev \
+	pypy \
+	pypy-dev \
+	cython \
+	cython3 \
+	build-essential \
+	python-nose \
+	python3-nose \
+	python-serial \
+	python3-serial \
+	time \
+	git
