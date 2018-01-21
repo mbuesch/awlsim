db71d79b8c1ecf416a13e0ab607467e218496422
Michael Buesch <m@bues.ch>
2018-01-21 20:37:10 +0100

    core/hardware_params: Add address parameter descriptors
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/core/hardware_params.py
+++ awlsim/core/hardware_params.py
@@ -2,7 +2,7 @@
 #
 # AWL simulator - Hardware module parameters
 #
-# Copyright 2013-2017 Michael Buesch <m@bues.ch>
+# Copyright 2013-2018 Michael Buesch <m@bues.ch>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,6 +22,7 @@
 from __future__ import division, absolute_import, print_function, unicode_literals
 from awlsim.common.compat import *
 
+from awlsim.common.util import *
 from awlsim.common.exceptions import *
 
 
@@ -30,6 +31,8 @@ __all__ = [ "HwParamDesc",
 	    "HwParamDesc_str",
 	    "HwParamDesc_int",
 	    "HwParamDesc_bool",
+	    "HwParamDesc_outAddr",
+	    "HwParamDesc_inAddr",
 ]
 
 
@@ -99,6 +102,8 @@ class HwParamDesc_int(HwParamDesc):
 		self.maxValue = maxValue
 
 	def parse(self, value):
+		if not value.strip():
+			return self.defaultValue
 		try:
 			value = int(value)
 		except ValueError:
@@ -124,6 +129,8 @@ class HwParamDesc_bool(HwParamDesc):
 
 	def parse(self, value):
 		value = value.strip()
+		if not value:
+			return self.defaultValue
 		if value.lower() in ("true", "yes", "on"):
 			return True
 		if value.lower() in ("false", "no", "off"):
@@ -134,3 +141,61 @@ class HwParamDesc_bool(HwParamDesc):
 			raise self.ParseError("Value '%s' is not a valid boolean." %\
 				str(value))
 		return bool(value)
+
+class __HwParamDesc_addr(HwParamDesc):
+	"""Address hardware parameter descriptor."""
+
+	typeStr = None
+	ADDR_PREFIXES = None
+
+	def __init__(self, name,
+		     defaultValue=None, minValue=None, maxValue=None,
+		     description="", mandatory=False):
+		HwParamDesc.__init__(self, name, description, mandatory)
+		self.defaultValue = defaultValue
+		self.minValue = minValue
+		self.maxValue = maxValue
+
+	def _parseAddr(self, value):
+		if not value.strip():
+			return self.defaultValue
+		try:
+			v = value.split()
+			if len(v) != 2:
+				raise ValueError
+			if not any(strEqual(v[0], p, False) for p in self.ADDR_PREFIXES):
+				raise ValueError
+			o = v[1].split(".")
+			if len(o) != 2:
+				raise ValueError
+			byteOffset, bitOffset = int(o[0], 10), int(o[1], 10)
+			if byteOffset < 0 or bitOffset < 0 or bitOffset > 7:
+				raise ValueError
+			return byteOffset * 8 + bitOffset
+		except ValueError:
+			raise self.ParseError("Value '%s' is not a %s." % (
+					      str(value), self.typeStr))
+
+	def parse(self, value):
+		bitOffset = self._parseAddr(value)
+		if self.minValue is not None:
+			minBitOffset = self._parseAddr(self.minValue)
+			if bitOffset < minBitOffset:
+				raise self.ParseError("Address '%s' is too small." % value)
+		if self.maxValue is not None:
+			maxAddr = self._parseAddr(self.maxValue)
+			if bitOffset > maxBitOffset:
+				raise self.ParseError("Address '%s' is too big." % value)
+		return bitOffset
+
+class HwParamDesc_outAddr(__HwParamDesc_addr):
+	"""Output address hardware parameter descriptor."""
+
+	typeStr = "outputAddress"
+	ADDR_PREFIXES = ("Q", "A")
+
+class HwParamDesc_inAddr(__HwParamDesc_addr):
+	"""Input address hardware parameter descriptor."""
+
+	typeStr = "inputAddress"
+	ADDR_PREFIXES = ("I", "E")
