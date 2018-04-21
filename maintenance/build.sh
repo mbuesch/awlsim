ae02d22c115d2c18929c4b6e613d59dfdde07dbc
Michael Buesch <m@bues.ch>
2018-04-21 15:31:09 +0200

    buildscript: Disable Cython2 build by default
    Signed-off-by: Michael Buesch <m@bues.ch>

--- maintenance/build.sh
+++ maintenance/build.sh
@@ -12,42 +12,71 @@ if ! [ -x "$awlsim_base/awlsim-test" -a -x "$awlsim_base/setup.py" ]; then
 	exit 1
 fi
 
-opt_verbose=0
-if [ "$1" = "-v" ]; then
-	opt_verbose=1
-fi
 
-run()
+usage()
+{
+	echo "build.sh [OPTIONS]"
+	echo
+	echo " -h|--help     Show help"
+	echo " -v|--verbose  Verbose build"
+	echo " -f|--full     Full build; Cython2 and Cython3"
+}
+
+opt_verbose=0
+opt_full=0
+while [ $# -ge 1 ]; do
+	case "$1" in
+	-h|--help)
+		usage
+		exit 0
+		;;
+	-v|--verbose)
+		opt_verbose=1
+		;;
+	-f|--full)
+		opt_full=1
+		;;
+	esac
+	shift
+done
+
+
+do_build()
 {
 	nice -n 10 "$1" ./setup.py build &
 	RET=$!
 }
 
+build()
+{
+	local name="$1"
+	local interpreter="$2"
+
+	echo "Running $name build..."
+	if [ $opt_verbose -eq 0 ]; then
+		do_build "$interpreter" >/dev/null
+	else
+		do_build "$interpreter"
+	fi
+}
+
 cd "$awlsim_base"
-echo "Running build..."
-
 export AWLSIM_CYTHON_BUILD=1
-
-if [ $opt_verbose -eq 0 ]; then
-	run python2 >/dev/null
-else
-	run python2
+if [ $opt_full -ne 0 ]; then
+	build Cython2 python2
+	python2_build_pid=$RET
 fi
-python2_build_pid=$RET
-
-if [ $opt_verbose -eq 0 ]; then
-	run python3 >/dev/null
-else
-	run python3
-fi
+build Cython3 python3
 python3_build_pid=$RET
 
-if ! wait $python2_build_pid; then
-	echo "Python 2 build FAILED!"
-	exit 1
+if [ $opt_full -ne 0 ]; then
+	if ! wait $python2_build_pid; then
+		echo "Cython2 build FAILED!"
+		exit 1
+	fi
 fi
 if ! wait $python3_build_pid; then
-	echo "Python 3 build FAILED!"
+	echo "Cython3 build FAILED!"
 	exit 1
 fi
 
