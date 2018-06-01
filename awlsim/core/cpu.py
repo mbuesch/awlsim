3f1e75df594fa41bf036dce0f67ce21049425e4c
Michael Buesch <m@bues.ch>
2018-06-01 14:44:12 +0200

    cpu: Avoid OOB access in CPU dump
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/core/cpu.py
+++ awlsim/core/cpu.py
@@ -2,7 +2,7 @@
 #
 # AWL simulator - CPU
 #
-# Copyright 2012-2017 Michael Buesch <m@bues.ch>
+# Copyright 2012-2018 Michael Buesch <m@bues.ch>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -2251,14 +2251,17 @@ class S7CPU(object): #+cdef
 		def append(line):
 			ret.append((prefix if first else (' ' * len(prefix))) +\
 				   ' '.join(line))
-		end = maxLen + byteOffset
-		while i < end:
-			line.append("%02X" % memArray[i])
-			count += 1
-			if count >= 16:
-				append(line)
-				line, count, first = [], 0, False
-			i += 1
+		try:
+			end = maxLen + byteOffset
+			while i < end:
+				line.append("%02X" % memArray[i])
+				count += 1
+				if count >= 16:
+					append(line)
+					line, count, first = [], 0, False
+				i += 1
+		except IndexError as e:
+			pass # memArray out of bounds access.
 		if count:
 			append(line)
 		return ret
