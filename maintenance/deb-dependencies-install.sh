19c20ec1c85cd3038d0f2ad0a49e4d144f9c9bff
Michael Buesch <m@bues.ch>
2018-08-05 14:10:20 +0200

    deb-dependencies-install: Add devscripts
    Signed-off-by: Michael Buesch <m@bues.ch>

--- maintenance/deb-dependencies-install.sh
+++ maintenance/deb-dependencies-install.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
 exec apt install \
-	python \
-	python-dev \
-	python3 \
-	python3-dev \
-	pypy \
-	pypy-dev \
+	build-essential \
 	cython \
 	cython3 \
-	build-essential \
+	devscripts \
+	git \
+	pypy \
+	pypy-dev \
+	python \
 	python-cffi \
-	python3-cffi \
+	python-dev \
 	python-nose \
-	python3-nose \
-	python-serial \
-	python3-serial \
-	python-pyside \
-	python3-pyside \
 	python-pyqt5 \
+	python-pyside \
+	python-serial \
+	python3 \
+	python3-cffi \
+	python3-dev \
+	python3-nose \
 	python3-pyqt5 \
-	time \
-	git
+	python3-pyside \
+	python3-serial \
+	time
