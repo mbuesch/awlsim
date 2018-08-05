91e22a7f8377e8bd83a17910999a38bc0411de61
Michael Buesch <m@bues.ch>
2018-08-05 14:10:49 +0200

    deb-install: Remove Python2
    Signed-off-by: Michael Buesch <m@bues.ch>

--- maintenance/deb-install.sh
+++ maintenance/deb-install.sh
@@ -10,8 +10,14 @@ die()
 install()
 {
 	for i in "$@"; do
-		printf '%s' "$i" | grep -qe dbgsym && continue
-		echo "Installing $1 ..."
+		if printf '%s' "$i" | grep -qe dbgsym; then
+			continue
+		fi
+		if ! [ -f "$i" ]; then
+			echo "Warning: $i does not exist. Skipping..."
+			continue
+		fi
+		echo "Installing $i ..."
 		dpkg -i "$i" || die "FAILED: dpkg -i $i"
 	done
 }
@@ -19,12 +25,10 @@ install()
 basedir="$1"
 [ -d "$basedir" ] || die "Usage:  deb-install.sh PACKAGEDIR"
 
-for interp in python python3 cython cython3 pypy; do
+for interp in python3 cython3 pypy; do
 	install "$basedir"/$interp-awlsim_*_*.deb
 	install "$basedir"/$interp-awlsimhw-*_*_*.deb
-	if [ "$interp" != cython ] &&\
-	   [ "$interp" != cython3 ] &&\
-	   [ "$interp" != pypy ]; then
+	if [ "$interp" == "python3" ]; then
 		install "$basedir"/$interp-awlsim-gui_*_*.deb
 	fi
 done
