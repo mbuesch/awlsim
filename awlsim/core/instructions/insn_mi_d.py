e74899a2406e7df74419e88d87b7776c857653c3
Michael Buesch <m@bues.ch>
2018-08-14 15:01:35 +0200

    core/insn/mi-d: Use #+suffix-LL
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/core/instructions/insn_mi_d.py
+++ awlsim/core/instructions/insn_mi_d.py
@@ -57,6 +57,5 @@ class AwlInsn_MI_D(AwlInsn): #+cdef
 			s.A1, s.A0, s.OV = 0, 1, 0
 		else:
 			s.A1, s.A0, s.OV = 1, 0, 0
-		if diff > 0x7FFFFFFF or diff < -2147483648: #@nocy
-#@cy		if diff > 0x7FFFFFFFLL or diff < -2147483648LL:
+		if diff > 0x7FFFFFFF or diff < -2147483648: #+suffix-LL
 			s.OV, s.OS = 1, 1
