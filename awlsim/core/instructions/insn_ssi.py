67d88e81f0f253e6c862925f8d636edd9219e46f
Michael Buesch <m@bues.ch>
2018-08-14 22:56:28 +0200

    core/insn/ssi: Add Cython declarations
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/core/instructions/insn_ssi.py
+++ awlsim/core/instructions/insn_ssi.py
@@ -2,7 +2,7 @@
 #
 # AWL simulator - instructions
 #
-# Copyright 2012-2017 Michael Buesch <m@bues.ch>
+# Copyright 2012-2018 Michael Buesch <m@bues.ch>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -43,7 +43,7 @@ class AwlInsn_SSI(AwlInsn): #+cdef
 	def run(self): #+cdef
 #@cy		cdef S7StatusWord s
 #@cy		cdef int32_t accu1
-#@cy		cdef int64_t count
+#@cy		cdef uint32_t count
 
 		s = self.cpu.statusWord
 		accu1 = self.cpu.accu1.getSignedWord() #@nocy
@@ -54,7 +54,8 @@ class AwlInsn_SSI(AwlInsn): #+cdef
 			count = self.cpu.accu2.getByte()
 		if count <= 0:
 			return
-		count = min(count, 16)
+		if count > 16:
+			count = 16
 		s.A1, s.A0, s.OV = (accu1 >> (count - 1)) & 1, 0, 0
 		accu1 >>= count
 		self.cpu.accu1.setWord(accu1)
