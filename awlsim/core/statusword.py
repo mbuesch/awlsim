19925fa38fc2ffe3035a1c4f7089c93c0d610c9b
Michael Buesch <m@bues.ch>
2018-09-05 21:13:24 +0200

    core/stw: Propagate exceptions from getByBitNumber()
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/core/statusword.py
+++ awlsim/core/statusword.py
@@ -2,7 +2,7 @@
 #
 # AWL simulator - status word
 #
-# Copyright 2012-2017 Michael Buesch <m@bues.ch>
+# Copyright 2012-2018 Michael Buesch <m@bues.ch>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -165,7 +165,7 @@ class S7StatusWord(object): #+cdef
 			raise AwlSimError("Status word bit fetch '%d' "		#@nocy
 				"out of range" % bitNumber)			#@nocy
 
-#@cy	cdef _Bool getByBitNumber(self, uint8_t bitNumber):
+#@cy	cdef ExBool_t getByBitNumber(self, uint8_t bitNumber) except ExBool_val:
 #@cy		if bitNumber == 0:
 #@cy			return self.NER
 #@cy		elif bitNumber == 1:
