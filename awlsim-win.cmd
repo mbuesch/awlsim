f87bdab51cb193cebe9378995a4b5e78f78a9ce2
Michael Buesch <m@bues.ch>
2018-09-09 18:43:51 +0200

    awlsim-win wrapper: Add Cython option
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim-win.cmd
+++ awlsim-win.cmd
@@ -1,5 +1,7 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
+set basedir=%~dp0
+set awlsim_base=%basedir%
 
 set PYPROG=awlsim-gui
 for /D %%f in ( "progs\putty\*" ) do set PATH=%%f\putty;!PATH!
@@ -8,6 +10,15 @@ for /D %%f in ( "progs\putty\*" ) do set PATH=%%f\putty;!PATH!
 set PATH=%PATH%;C:\WINDOWS;C:\WINDOWS\SYSTEM32
 for /D %%f in ( "C:\PYTHON*" ) do set PATH=!PATH!;%%f
 for /D %%f in ( "%USERPROFILE%\AppData\Local\Programs\Python\Python*" ) do set PATH=!PATH!;%%f;%%f\Scripts
+
+
+rem Set Cython paths
+if not exist %awlsim_base%\build goto no_cython
+for /D %%f in ( "%awlsim_base%\build\lib.*-*-3.*" ) do set PYTHONPATH=%%f\;!PYTHONPATH!
+set PYTHONPATH=.;%PYTHONPATH%
+set AWLSIM_CYTHON=1
+echo PYTHONPATH=%PYTHONPATH%
+:no_cython
 
 
 py -h >NUL 2>&1
