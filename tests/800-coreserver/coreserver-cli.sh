4cc177f0df110ae61ed0de96bb01d5a8d29a9fd0
Michael Buesch <m@bues.ch>
2018-09-18 19:16:29 +0200

    tests/coreserver: Add basic MemoryArea test
    Signed-off-by: Michael Buesch <m@bues.ch>

--- tests/800-coreserver/coreserver-cli.sh
+++ tests/800-coreserver/coreserver-cli.sh
@@ -15,5 +15,16 @@ sh_test()
 			--connect-to localhost:$(get_port)
 	done
 
+	infomsg "----- Testing MemoryArea accesses"
+	run_test "$interpreter" "$basedir/000-base/EXAMPLE.awlpro" \
+		--spawn-backend --interpreter "$interpreter" \
+		--connect-to localhost:$(get_port) \
+		--mem-read E:1:8 --mem-read A:2:16 --mem-read M:3:32 \
+		--mem-read L:4:8 --mem-read DB:1:5:16 --mem-read T:10 \
+		--mem-read Z:10 --mem-read STW \
+		--mem-write E:50:8:1 --mem-write A:51:16:2 --mem-write M:52:32:3 \
+		--mem-write DB:1:5:16:5 --mem-write T:0:0 \
+		--mem-write Z:1:0
+
 	infomsg -n "--- Finished coreserver tests "
 }
