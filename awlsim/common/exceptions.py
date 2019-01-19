bb07f6a0dd0b255b2082ce9c5c9893ab3e351233
Michael Buesch <m@bues.ch>
2019-01-19 18:12:52 +0100

    exceptions: Add method to fetch message
    Signed-off-by: Michael Buesch <m@bues.ch>

--- awlsim/common/exceptions.py
+++ awlsim/common/exceptions.py
@@ -2,7 +2,7 @@
 #
 # AWL simulator - Exceptions
 #
-# Copyright 2012-2018 Michael Buesch <m@bues.ch>
+# Copyright 2012-2019 Michael Buesch <m@bues.ch>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -58,6 +58,9 @@ class AwlSimError(Exception):
 		self.sourceId = sourceId
 		self.sourceName = sourceName
 		self.seenByUser = False
+
+	def getMessage(self):
+		return self.message
 
 	def setCpu(self, cpu):
 		self.cpu = cpu
