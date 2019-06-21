8d33512acbea957502a74c5491eb76e12a1bb7a8
Michael Buesch <m@bues.ch>
2019-06-21 18:03:18 +0200

    python unittests: Run coverage
    Signed-off-by: Michael Buesch <m@bues.ch>

--- tests/awlsim_tstlib.py
+++ tests/awlsim_tstlib.py
@@ -2,6 +2,14 @@ from __future__ import division, absolute_import, print_function, unicode_litera
 
 from unittest import TestCase
 
+__all__ = [
+	"TestCase",
+	"initTest",
+]
+
 def initTest(testCaseFile):
 	from os.path import basename
 	print("(test case file: %s)" % basename(testCaseFile))
+
+# Run code coverage metrics, if enabled.
+import awlsim_loader.coverage_helper
