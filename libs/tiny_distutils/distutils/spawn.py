3edae30d3c82f6c40afacc3fa52f2bb33b53f2a3
Michael Buesch <m@bues.ch>
2020-02-09 15:54:35 +0100

    tiny_distutils: Add support for absolute paths
    Signed-off-by: Michael Buesch <m@bues.ch>

--- libs/tiny_distutils/distutils/spawn.py
+++ libs/tiny_distutils/distutils/spawn.py
@@ -1,15 +1,20 @@
 __all__ = [ "find_executable", ]
 
 import os
+import os.path
 
 def find_executable(executable, path=None):
 	if executable:
-		import os.path
-		if path is None:
-			path = os.getenv("PATH", "")
-		for p in path.split(":"):
-			fullpath = os.path.join(p, executable)
-			if os.access(fullpath, os.X_OK) and\
-			   not os.path.isdir(fullpath):
-				return fullpath
+		if executable.startswith(os.path.sep):
+			if (os.access(executable, os.X_OK) and
+			    not os.path.isdir(executable)):
+				return executable
+		else:
+			if path is None:
+				path = os.getenv("PATH", "")
+			for p in path.split(":"):
+				fullpath = os.path.join(p, executable)
+				if (os.access(fullpath, os.X_OK) and
+				    not os.path.isdir(fullpath)):
+					return fullpath
 	return None
