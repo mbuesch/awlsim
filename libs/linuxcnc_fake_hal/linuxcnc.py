fbaaa918db9214d08c02fd195bb7bb78424478f4
Michael Buesch <m@bues.ch>
2020-02-14 21:47:03 +0100

    hwmod/linuxcnc: Trigger estop after exceptions
    Fixes #21
    
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ libs/linuxcnc_fake_hal/linuxcnc.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# LinuxCNC fake Python module for unit testing
+#
+# Copyright 2020 Michael Buesch <m@bues.ch>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+from __future__ import division, absolute_import, print_function, unicode_literals
+
+
+STATE_ESTOP = 100
+
+class command(object):
+	def state(self, s):
+		assert s == STATE_ESTOP
+
+	def wait_complete(self, timeout=1.0):
+		assert timeout >= 0.0
