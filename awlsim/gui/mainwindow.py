c20273f92080d3805803adea194e3543d5239cb8
Michael Buesch <m@bues.ch>
2022-08-05 21:44:33 +0200

    Update copyright date

--- awlsim/gui/mainwindow.py
+++ awlsim/gui/mainwindow.py
@@ -2,7 +2,7 @@
 #
 # AWL simulator - GUI main window
 #
-# Copyright 2012-2020 Michael Buesch <m@bues.ch>
+# Copyright 2012-2022 Michael Buesch <m@bues.ch>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -1026,7 +1026,7 @@ class MainWindow(QMainWindow):
 		QMessageBox.about(self, "About Awlsim PLC",
 			"Awlsim PLC version %s\n"
 			"\n"
-			"Copyright 2012-2020 Michael Büsch <m@bues.ch>\n"
+			"Copyright 2012-2022 Michael Büsch <m@bues.ch>\n"
 			"\n"
 			"Project home:  %s\n"
 			"\n"
